b70b1a29a23c901335ec2da28d7a76fd051483c6
Mainemirror <stoppleasejesus@gmail.com>
2019-12-04 16:41:51 -0500

    new

---
+++ build/precache-manifest.549030936fbc1748d3b5b1298c12b561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517973ef215f56c233bdaac2e8805950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "77faa5a30479ee997c90",
+    "url": "/static/css/main.70a1d134.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "77faa5a30479ee997c90",
+    "url": "/static/js/main.f71f0c93.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Miguel Gomez',
+    username: 'Dimension',
+    email: 'Waterbottle@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Elizabeth Jackson',
+    username: 'Moochie',
+    email: 'Pencil@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Alexa Stamford',
+    username: 'IloveAmazon',
+    email: 'Amazon@amazon.com'
+  },
+  {
+    id: 4,
+    name: 'Tanesha Maxwell',
+    username: 'Hoodie',
+    email: 'Fra@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Brown',
+    username: 'Dope',
+    email: 'Smellsgoodtome@cnn.biz'
+  },
+  {
+    id: 6,
+    name: 'Freddie Curger ',
+    username: 'Nightmare',
+    email: 'Elmstreet@live'
+  },
+  {
+    id: 7,
+    name: 'Forrest Gump',
+    username: 'Chocolates',
+    email: 'Iwasrunning@bubbagump.food'
+  },
+  {
+    id: 8,
+    name: 'Rick Sanchez',
+    username: 'Pickle Rick',
+    email: 'Stupidjerry@cn.biz'
+  },
+  {
+    id: 9,
+    name: 'Grimm Reaper',
+    username: 'Death',
+    email: 'Dontbeg@death.net'
+  },
+  {
+    id: 10,
+    name: 'Krampus',
+    username: 'Krampus',
+    email: 'Krampus@k.biz'
+  }
+];
