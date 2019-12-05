cd11da70a0f2f8fe147a2d2c333d15d92628139d
Mainemirror <stoppleasejesus@gmail.com>
2019-12-05 12:10:19 -0500

    Updates

---
+++ precache-manifest.0fc53b96318a90169deac6351d0f0165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e40bf10840eaf41196ec76a6d1b2dd8",
+    "url": "/robofriendsmine/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriendsmine/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "df71c8fbe22ce1ac423b",
+    "url": "/robofriendsmine/static/css/main.4c20535d.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriendsmine/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "df71c8fbe22ce1ac423b",
+    "url": "/robofriendsmine/static/js/main.668e390e.chunk.js"
+  },
+  {
+    "revision": "abb0c1f7d41374fc37d1",
+    "url": "/robofriendsmine/static/js/runtime-main.2933d354.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsmine/static/media/SEGA.12a3db8b.woff"
+  }
+]);
